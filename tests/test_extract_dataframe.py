36c8e93f87e773333d23b21f0c905a84af5fc8b9
unknown <hikmaburhan01@gmail.com>
2022-04-27 20:08:50 +0300

    test extract dataframe

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 from extract_dataframe import TweetDfExtractor
 from extract_dataframe import read_json
 
-_, tweet_list = read_json("./data/Economic_Twitter_Data.json")
+_, tweet_list = read_json("../data/Economic_Twitter_Data.json")
 
 columns = ['created_at', 'source', 'original_text', 'clean_text', 'sentiment', 'polarity', 'subjectivity', 'lang', 'favorite_count', 'retweet_count',
            'original_author', 'screen_count', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
